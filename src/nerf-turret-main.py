c7de0f045ac29bbbd30c48ed56235f0b0bbaff0d
jyost <jonathan.yost@arrow.com>
2017-08-24 13:20:13 -0600

    + ugly mug ~ main

--- src/nerf-turret-main.py
+++ src/nerf-turret-main.py
@@ -8,9 +8,9 @@ import numpy as np
 print(cv2.__version__)
 print(np.__version__)
 
-img = cv2.imread(
-    "C:\\Users\\A84690\\Documents\\IOT project\\Diagrams\\ENV_wiring_version_1_bb.png",
-    0)
+file_path = "C:\\Users\\A84690\\Documents\\Dev\\Comp_Vis\\nerf-turret\\images\\ugly_mug.png"
+
+img = cv2.imread(file_path, 0)
 cv2.imshow('diagram', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
